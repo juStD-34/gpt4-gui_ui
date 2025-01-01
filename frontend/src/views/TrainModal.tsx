55bb5c892944eb47bfe881548ecff9254d92c34b
sangdo1 <sang.do1@onemount.com>
2025-01-02 03:46:03 +0700

    add call api train

--- frontend/src/views/TrainModal.tsx
+++ frontend/src/views/TrainModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import {
   Layout,
   Form,
@@ -9,9 +10,14 @@ import {
   Upload,
   Button,
   Tabs,
+  message,
 } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
-import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { json } from "@codemirror/lang-json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { API_ENDPOINTS } from "../const";
 
 const { Sider, Content } = Layout;
 const { TextArea } = Input;
@@ -20,10 +26,175 @@ const { TabPane } = Tabs;
 export default function TrainingInterface() {
   const [form] = Form.useForm();
   const [activeTab, setActiveTab] = useState("jsonContent");
+  const [jsonContent, setJsonContent] = useState("");
+  const [options, setOptions] = useState([
+    { value: "codeT5Small", label: "CodeT5 Small" },
+    { value: "codeT5Base", label: "CodeT5 Base" },
+    { value: "codeT5Large", label: "CodeT5 Large" },
+  ]);
+  // const [jsonAnalysis, setJsonAnalysis] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-  const onFinish = (values: any) => {
+  const onFinish = async (values: any) => {
     console.log("Form values:", values);
+
+    // Tạo FormData để gửi file và các thông tin khác
+    const formData = new FormData();
+
+    // Kiểm tra và append file vào formData
+    if (values.trainingSet && values.trainingSet.file) {
+      console.log(values.trainingSet.file, "FILE")
+      formData.append("file", values.trainingSet.file.originFileObj);
+    }
+
+    // Append các dữ liệu khác vào formData
+    formData.append("epochs", values.epochs);
+    formData.append("modelType", values.modelType);
+    formData.append("loggingDir", values.loggingDirectory || "");
+    formData.append("outputDir", values.outputDirectory || "");
+    formData.append("configId", values.configId || 1)
+    // formData.append("trainingRequest", JSON.stringify({
+    //   epochs: values.epochs,
+    //   modelType: values.modelType,
+    //   loggingDirectory: values.loggingDirectory,
+    //   outputDirectory: values.outputDirectory,
+    //   configId: values.configId
+    // }));
+    
+    console.log(formData)
+
+    try {
+      const response = await fetch(API_ENDPOINTS.TRAIN, {
+        method: "POST",
+        body: formData, // Gửi FormData thay vì JSON
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      message.success("Training started successfully!");
+      console.log("API Response:", data);
+    } catch (error) {
+      console.error("API Error:", error);
+      message.error("Failed to start training");
+    }
   };
+
+
+  const readFileContent = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        console.log("File loaded successfully");
+        resolve(event.target?.result as string);
+      };
+
+      reader.onerror = (error) => {
+        console.error("Error reading file:", error);
+        reject(error);
+      };
+
+      reader.readAsText(file);
+    });
+  };
+
+  const handleFileChange = async (info: any) => {
+    // const file = event.target.files[0];
+    const file = info.fileList[0];
+
+    console.log("File change event:", file);
+    if (file && file.originFileObj) {
+      try {
+        const content = await readFileContent(file.originFileObj);
+
+        try {
+          const jsonContent = JSON.parse(content);
+          setJsonContent(JSON.stringify(jsonContent, null, 2)); // Pretty print JSON
+          message.success(
+            `${file.name} file uploaded and parsed successfully.`
+          );
+        } catch (error) {
+          console.error("JSON parsing error:", error);
+          message.error(`Failed to parse JSON: ${error.message}`);
+        }
+      } catch (error) {
+        console.error("File reading error:", error);
+        message.error(`Failed to read file: ${error}`);
+      }
+    } else {
+      console.log("No file selected or file removed");
+    }
+  };
+
+  const parseJsonAnalysis = (jsonContent: string) => {
+    try {
+      const jsonArray = JSON.parse(jsonContent);
+
+      const aURLCounts: { [key: string]: { summaries: Set<string> } } = {};
+
+      jsonArray.forEach((item: { aURL: string; bSummary: string }) => {
+        const { aURL, bSummary } = item;
+        if (aURL in aURLCounts) {
+          aURLCounts[aURL].summaries.add(bSummary);
+        } else {
+          aURLCounts[aURL] = { summaries: new Set([bSummary]) };
+        }
+      });
+
+      // Sort aURLs by the number of scenarios in descending order
+      const sortedAURLCounts = Object.entries(aURLCounts).sort(
+        (a, b) => b[1].summaries.size - a[1].summaries.size
+      );
+
+      const analysisResult = sortedAURLCounts
+        .map(([aURL, data]) => {
+          const summaryList = Array.from(data.summaries)
+            .map((summary) => `- ${summary}`)
+            .join("\n");
+          return `${aURL} (${data.summaries.size} test scenarios)\n${summaryList}`;
+        })
+        .join("\n\n");
+
+      return analysisResult;
+      // setJsonAnalysis(analysisResult);
+    } catch (error) {
+      console.error("Error analyzing JSON:", error);
+    }
+  };
+
+  const handleSearch = (value: string) => {
+    setInputValue(value);
+  };
+
+  const handleInputKeyDown = (event: React.KeyboardEvent) => {
+    if (
+      event.key === "Enter" &&
+      inputValue &&
+      !options.some((option) => option.value === inputValue)
+    ) {
+      const newOption = { value: inputValue, label: inputValue };
+      setOptions((prevOptions) => [...prevOptions, newOption]);
+      form.setFieldsValue({ modelType: inputValue });
+      setInputValue("");
+    }
+  };
+
+  const handleChange = (value: string) => {
+    console.log("Selected or input value:", value);
+  };
+
+  useEffect(() => {
+    form.setFieldsValue({
+      trainingSet: undefined,
+      epochs: 1,
+      modelType: undefined,
+      loggingDirectory: undefined,
+      outputDirectory: undefined,
+    });
+  }, []);
 
   return (
     <Layout className="min-h-screen">
@@ -44,9 +215,18 @@ export default function TrainingInterface() {
             name="trainingSet"
             className="mb-6"
           >
-            <Upload>
+            <Upload
+              accept=".json,.jsonl"
+              beforeUpload={() => false}
+              onChange={handleFileChange}
+            >
               <Button icon={<UploadOutlined />}>Choose File</Button>
             </Upload>
+            {/* <input
+              type="file"
+              accept=".json,.jsonl"
+              onChange={handleFileChange}
+            /> */}
           </Form.Item>
 
           {/* Training Epochs */}
@@ -54,13 +234,20 @@ export default function TrainingInterface() {
             <InputNumber className="w-full" min={1} defaultValue={1} />
           </Form.Item>
 
+          <Input.Search placeholder="Search" enterButton />
+
           {/* Model Type */}
           <Form.Item label="Model Type" name="modelType" className="mb-6">
-            <Select defaultValue="codeT5Small">
-              <Select.Option value="codeT5Small">CodeT5 Small</Select.Option>
-              <Select.Option value="codeT5Base">CodeT5 Base</Select.Option>
-              <Select.Option value="codeT5Large">CodeT5 Large</Select.Option>
-            </Select>
+            <Select
+              showSearch
+              allowClear
+              placeholder="Select or input a model type"
+              optionFilterProp="children"
+              options={options}
+              onSearch={handleSearch}
+              onChange={handleChange}
+              onInputKeyDown={handleInputKeyDown}
+            />
           </Form.Item>
 
           {/* Logging Directory */}
@@ -110,7 +297,15 @@ export default function TrainingInterface() {
 
       <Content className="bg-white p-6">
         <Tabs activeKey={activeTab} onChange={setActiveTab} className="mb-6">
-          <TabPane tab="JSON Content" key="jsonContent" />
+          <TabPane
+            tab={
+              <span>
+                <FontAwesomeIcon icon={faCode} className="mr-2" />
+                JSON Content
+              </span>
+            }
+            key="jsonContent"
+          />
           <TabPane tab="JSON Analysis" key="jsonAnalysis" />
           <TabPane tab="Training Logs" key="trainingLogs" />
         </Tabs>
@@ -118,14 +313,22 @@ export default function TrainingInterface() {
         {/* Content area for the selected tab */}
         <div className="min-h-[500px]">
           {activeTab === "jsonContent" && (
-            <div className="text-gray-500">
-              JSON content will be displayed here
-            </div>
+            <CodeMirror
+              value={jsonContent}
+              height="500px"
+              extensions={[json()]}
+              // theme="dark"
+              editable={false}
+              className="border border-gray-300 rounded"
+            />
           )}
           {activeTab === "jsonAnalysis" && (
-            <div className="text-gray-500">
-              JSON analysis will be displayed here
-            </div>
+            <CodeMirror
+              value={parseJsonAnalysis(jsonContent)}
+              height="500px"
+              editable={false}
+              className="border border-gray-300 rounded"
+            />
           )}
           {activeTab === "trainingLogs" && (
             <div className="text-gray-500">
